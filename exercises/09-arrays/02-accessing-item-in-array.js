41aacf360fc99170b77851397f483f05257ad984
matinaspatsos <matinaspatsos@gmail.com>
2022-10-23 16:32:04 -0400

    Arrays exercises

---
+++ exercises/09-arrays/02-accessing-item-in-array.js
@@ -0,0 +1,9 @@
+const cars = ["BMW", "Honda", "Civic"]; // Do not change this line
+
+/**
+ * Create an array called "myCar".
+ * It should be equal to the first item in the array of cars.
+ * Solve this problem without using destructuring.
+ */
+
+// WRITE YOUR ANSWER BELOW THIS LINE
---
+++ exercises/09-arrays/03-destructuring.js
@@ -0,0 +1,12 @@
+const cars = ["BMW", "Honda", "Civic"]; // Do not change this line
+
+/**
+ * This problem is a repeat of the last problem,
+ * except you will solve this problem with destructuring.
+ *
+ * Create an array called "myCar".
+ * It should be equal to the first item in the array of cars.
+ * Solve this problem without using destructuring.
+ */
+
+// WRITE YOUR ANSWER BELOW THIS LINEs
