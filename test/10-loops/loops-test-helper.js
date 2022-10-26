3650910fa4ff8b746f52dd822579aa69d8d22451
Matina Patsos <matina@deepbluecommunications.com>
2022-10-26 18:38:10 -0400

    Loops exercise
    Loops exercises Part 1

---
+++ test/10-loops/loops-test-helper.js
@@ -0,0 +1,9 @@
+import solvingProblems from "../../exercises/10-loops/00-solving-problems-with-functions.js";
+
+import arrayLength from "../../exercises/10-loops/01-array-length.js";
+
+import logWithWhile from "../../exercises/10-loops/02-while-loop.js";
+
+import logArrayWithFor from "../../exercises/10-loops/03-for-loop.js";
+
+export { solvingProblems, arrayLength, logWithWhile, logArrayWithFor };
