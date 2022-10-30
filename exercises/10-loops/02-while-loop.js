c328164001680a2cae07aa48612016d2597ef367
matinaspatsos <matinaspatsos@gmail.com>
2022-10-30 12:22:57 -0400

    Update array instructions

--- exercises/10-loops/02-while-loop.js
+++ exercises/10-loops/02-while-loop.js
@@ -1,7 +1,7 @@
 /**
  * Using a while loop, log the following numbers:
  * 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
- * 
+ *
  * @example
  * logArrayWithWhile();
  * // 10
@@ -17,10 +17,8 @@
  */
 
 const logWithWhile = () => {
-	// WRITE YOUR ANSWER HERE
+  // WRITE YOUR ANSWER HERE
 };
-
-logWithWhile();
 
 // IGNORE THIS BELOW. It is for the tests.
 
