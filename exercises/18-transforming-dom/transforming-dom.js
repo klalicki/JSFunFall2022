3aab4f11d123f638931a74c7c3b0f362ca6008de
Kristoff Lalicki <kristofflalicki@gmail.com>
2022-11-30 21:12:21 -0500

    finished exercise 18

--- exercises/18-transforming-dom/transforming-dom.js
+++ exercises/18-transforming-dom/transforming-dom.js
@@ -4,5 +4,14 @@
  * You may modify the HTML to add ids, classes, data attributes, etc.
  */
 (function () {
-  // Put your answers in here
+  document.querySelector("#ex1").src =
+    "https://media.giphy.com/media/3oKIPnAiaMCws8nOsE/giphy.gif";
+  document.querySelector("#ex2").href =
+    "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
+  document.querySelector("#ex3").textContent = "I am victorious!";
+  document.querySelector("#ex4").style.backgroundColor = "lightYellow";
+  document.querySelector("#ex5").classList.add("text-primary");
+  if (document.querySelector("#ex6a").classList.contains("btn-primary")) {
+    document.querySelector("#ex6b").textContent = "✓ blue";
+  }
 })();
