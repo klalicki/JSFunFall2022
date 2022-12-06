12d04d2bbe7298a26ec0b028c09ca9499a0cd75e
Kristoff Lalicki <kristofflalicki@gmail.com>
2022-12-05 19:51:57 -0500

    submitting ex 18

--- exercises/18-transforming-dom/transforming-dom.js
+++ exercises/18-transforming-dom/transforming-dom.js
@@ -4,14 +4,24 @@
  * You may modify the HTML to add ids, classes, data attributes, etc.
  */
 (function () {
-  document.querySelector("#ex1").src =
+  document.querySelector('[data-exercise="1"]').src =
     "https://media.giphy.com/media/3oKIPnAiaMCws8nOsE/giphy.gif";
-  document.querySelector("#ex2").href =
+
+  document.querySelector('[data-exercise="2"]').href =
     "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
-  document.querySelector("#ex3").textContent = "I am victorious!";
-  document.querySelector("#ex4").style.backgroundColor = "lightYellow";
-  document.querySelector("#ex5").classList.add("text-primary");
-  if (document.querySelector("#ex6a").classList.contains("btn-primary")) {
-    document.querySelector("#ex6b").textContent = "✓ blue";
+
+  document.querySelector('[data-exercise="3"]').textContent =
+    "I am victorious!";
+  document.querySelector('[data-exercise="4"]').style.backgroundColor =
+    "lightYellow";
+  document.querySelector('[data-exercise="5"]').classList.add("text-primary");
+  document.querySelector('[data-exercise="6"]').style.display = "none";
+  document.querySelector('[data-exercise="7"]').classList.remove("hidden");
+  if (
+    document
+      .querySelector('[data-exercise="8a"]')
+      .classList.contains("btn-primary")
+  ) {
+    document.querySelector('[data-exercise="8b"]').textContent = "&check; blue";
   }
 })();
