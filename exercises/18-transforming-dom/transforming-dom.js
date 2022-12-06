c86874b5de70414646f134031acc5198a7a2e661
pmermod <93401090+pmermod@users.noreply.github.com>
2022-12-05 19:12:52 -0500

    Problems 18

--- exercises/18-transforming-dom/transforming-dom.js
+++ exercises/18-transforming-dom/transforming-dom.js
@@ -5,4 +5,25 @@
  */
 (function () {
   // Put your answers in here
+  const img = document.querySelector("img");
+  img.src = "https://media.giphy.com/media/3oKIPnAiaMCws8nOsE/giphy.gif";
+  //
+  const link = document.querySelector("#jsref");
+  link.href = "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
+  //
+  const text = document.querySelector("#textchange");
+  text.textContent = "I am victorious!";
+  //
+  const bgColor = document.querySelector("#colorchange");
+  bgColor.style.backgroundColor = "#bbbbbb";
+  //
+  //const TxtClassColor = document.querySelector("text-info");
+  //
+  const divToHide = document.querySelector("#hide");
+  divToHide.style.display = "none";
+
+  const myButton = document.querySelector("#button1");
+  if (myButton.classList.contains("btn-primary")) {
+    myButton.textContent = "checkmark blue";
+  }
 })();
