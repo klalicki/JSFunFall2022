ec0f1eae145d25de0eafde72e5abcf01c1d3fbfa
klalicki <32229646+klalicki@users.noreply.github.com>
2022-12-14 20:43:56 -0500

    submitting ex 22

--- exercises/22-identify-elements/indentify-elements.js
+++ exercises/22-identify-elements/indentify-elements.js
@@ -1,5 +1,4 @@
 !(function () {
-  console.log(document.querySelectorAll(".pricing-plan button"));
   /**
    * When the user clicks on one of the "Select" buttons to select a plan,
    * display the following message in the aqua green notification at the top of the screen,
@@ -14,4 +13,23 @@
    * You can also make the "x" icon dismiss the notification.
    */
   // Write your answer here
+  //event handler for purchase buttons
+  const handlePurchase = (event) => {
+    //get the text from the button's data attribute
+    const dataPlanName = event.target.getAttribute("data-plan-name");
+    const dataMessageString = `Thank you for purchasing the ${dataPlanName} plan`;
+    //set the notification to show the correct text
+    document.querySelector("#notification").textContent = dataMessageString;
+    // display the notification
+    document.querySelector(".notification").classList.remove("is-hidden");
+  };
+  // event handler for notification close button
+  const handleClose = (event) => {
+    document.querySelector(".notification").classList.add("is-hidden");
+  };
+  //add event listeners
+  document.querySelector(".delete").addEventListener("click", handleClose);
+  document.querySelectorAll(".pricing-plan button").forEach((element) => {
+    element.addEventListener("click", handlePurchase);
+  });
 })();
