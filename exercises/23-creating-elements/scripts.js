fdaaa64edeb8ef6e68b20542d4e7a43e0d2f4058
Kristoff Lalicki <kristofflalicki@gmail.com>
2022-12-19 17:54:02 -0500

    added comments to JS on ex23

--- exercises/23-creating-elements/scripts.js
+++ exercises/23-creating-elements/scripts.js
@@ -1,14 +1,17 @@
 (() => {
   document.querySelector("#btn-submit").addEventListener("click", (e) => {
     e.preventDefault();
+    //get values from form
     const color = document.querySelector("#input-color").value;
     const content = document.querySelector("#input-content").value;
+    //create a new element and set its text and color
     const newElement = document.createElement("div");
     newElement.textContent = content;
     newElement.style.backgroundColor = color;
+    newElement.addEventListener("click", (e) => {
+      e.target.remove();
+    });
+    //add element to screen
     document.querySelector("#item-container").appendChild(newElement);
-    newElement.addEventListener('click', (e) => {
-      e.target.remove()
-    })
   });
 })();
