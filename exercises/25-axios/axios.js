8559504da8d94550b1d7c138288653a5d69a8f77
Kristoff Lalicki <kristofflalicki@gmail.com>
2022-12-21 20:50:43 -0500

    submitting ex 25

--- exercises/25-axios/axios.js
+++ exercises/25-axios/axios.js
@@ -12,4 +12,27 @@
    *    "message": "https://images.dog.ceo/breeds/poodle-toy/n02113624_9550.jpg"
    * }
    */
+  const getNewDog = () => {
+    //url of the Dog API
+    const apiUrl = "https://dog.ceo/api/breeds/image/random";
+    axios({
+      url: apiUrl,
+      method: "get",
+      headers: { "content-type": "application/json" },
+    })
+      // .get(apiUrl)
+      .then((response) => {
+        //extract URL from API response
+        const newDogUrl = response.data.message;
+        //update src of image tag
+        document.querySelector("#image").src = newDogUrl;
+      })
+      .catch((err) => {
+        //catch error
+        console.log(err);
+      });
+  };
+  //add event listener to button
+  document.querySelector("button").addEventListener("click", getNewDog);
+  
 })();
