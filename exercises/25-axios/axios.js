e591eaa54b91c2ad288f303e87fd3c0aa5d4c5dd
pmermod <93401090+pmermod@users.noreply.github.com>
2023-01-03 13:34:46 -0500

    Problem 25 Axios - Hal

--- exercises/25-axios/axios.js
+++ exercises/25-axios/axios.js
@@ -12,4 +12,15 @@
    *    "message": "https://images.dog.ceo/breeds/poodle-toy/n02113624_9550.jpg"
    * }
    */
+  /*
+  axios({
+    url: "https://dog.ceo/api/breeds/image/random",
+    method: "GET",
+  });
+  */
+  const button = document.querySelector("button");
+  button.addEventListener("click", (e) => {
+    const image = document.querySelector("#image");
+    image.src = "https://dog.ceo/api/breeds/image/random";
+  });
 })();
