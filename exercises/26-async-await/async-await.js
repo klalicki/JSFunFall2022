7fcec1b4cef338ddc655911cd2b3520108ba329e
Kristoff Lalicki <kristofflalicki@gmail.com>
2023-01-04 20:54:37 -0500

    submit assigmnents 26&27

--- exercises/26-async-await/async-await.js
+++ exercises/26-async-await/async-await.js
@@ -8,4 +8,20 @@
    * This is the API you will be using. The method will be GET.
    * https://ron-swanson-quotes.herokuapp.com/v2/quotes
    */
+  const getQuote = async () => {
+    try {
+      //get quote data from the API
+      const { data } = await axios.get(
+        "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
+      );
+      //extract the quote string from the array the API returns
+      const newQuote = data[0];
+      //replace the quote on the webpage
+      document.querySelector("#quote").textContent = newQuote;
+    } catch {
+      //error?
+    }
+  };
+  //add event listener to button
+  document.querySelector("button").addEventListener("click", getQuote);
 })();
--- exercises/27-axios/axios.js
+++ exercises/27-axios/axios.js
@@ -16,5 +16,49 @@
    * https://rickandmortyapi.com/api/character/2
    *
    * Use the AXIOS library to make AJAX requests.
+   *
    */
+  const populateList = async () => {
+    const { data } = await axios.get(
+      "https://rickandmortyapi.com/api/character"
+    );
+    const pageCount = data.info.pages;
+    for (let i = 1; i <= pageCount; i++) {
+      const queryString = `https://rickandmortyapi.com/api/character?page=${i}`;
+      const { data } = await axios.get(queryString);
+      const characterList = data.results;
+      const charSelectBox = document.querySelector("#dropdown");
+      characterList.forEach((char) => {
+        //extract id and name from element
+        const curId = char.id;
+        const curName = char.name;
+        //create a new element with the name and id from element
+        const elem = document.createElement("option");
+        elem.value = curId;
+        elem.textContent = curName;
+        //add the element to the dropdown box
+        charSelectBox.appendChild(elem);
+      });
+    }
+  };
+
+  const handleListChange = async () => {
+    //get the value of the dropdown box
+    const charSelectBox = document.querySelector("#dropdown");
+    const id = charSelectBox.value;
+    //get the character from the API using the ID
+    const { data } = await axios.get(
+      `https://rickandmortyapi.com/api/character/${id}`
+    );
+    //extract character name and image URL from the API call result
+    const imgUrl = data.image;
+    const charName = data.name;
+    //update the page elements with the character
+    document.querySelector("#get-schwifty").src = imgUrl;
+    document.querySelector("#title-head").textContent = charName;
+  };
+  populateList();
+
+  const charSelectBox = document.querySelector("#dropdown");
+  charSelectBox.addEventListener("change", handleListChange);
 })();
